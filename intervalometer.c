b1021116241db7e030bf1d2346106b6e801118fa
Luna Hart <luna@night.horse>
2024-06-17 19:08:35 -0700

    Fix: Update input cb func signature

--- intervalometer.c
+++ intervalometer.c
@@ -494,7 +494,8 @@ static void flipvalo_run_state_init(struct flipvalo_run_state* fv_run_state) {
     fv_run_state->tick_cur = 0;
 }
 
-static void input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
+static void input_callback(InputEvent* input_event, void* event_queue_void) {
+    FuriMessageQueue* event_queue = (FuriMessageQueue*) event_queue_void;
     furi_assert(event_queue);
     struct plugin_event event = {.type = EventTypeKey, .input = *input_event };
     furi_message_queue_put(event_queue, &event, FuriWaitForever);
